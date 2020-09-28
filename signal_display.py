e939f1839dd6d6ef939dab116c09c5a8f17c17ef
yinda <da_yin@139.com>
2020-09-28 22:31:34 +0800

    显示函数

--- signal_display.py
+++ signal_display.py
@@ -2,26 +2,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class display_signal:
-    def __init__(self, t, x=1, y=1):
-        plt.figure()
+    def __init__(self, fs, N, x=1, y=1, title=''):
+        plt.figure(), plt.suptitle(title)
+
+        self.fs = fs
+        self.N = N
+        self.t_t = np.linspace(0, N/fs, N)                 # 时域坐标
+        self.t_s = np.linspace(-N//2, N//2, N) * fs/N      # 频域坐标
+
         self.count = 1
-        self.t = t
-        self.x = x
+        self.x = x      
         self.y = y
     
     def display_time(self, signal, title=''):
         plt.subplot(self.x, self.y, self.count)
         self.count = self.count + 1
 
-        plt.plot(self.t, signal), plt.title(title)
+        plt.plot(self.t_t, signal), plt.title(title)
 
     
     def display_sprcturm(self, signal, title=''):
-        signal_fft = np.fft.fft(signal)
+        signal_fft = np.fft.fftshift(np.fft.fft(signal))    # 傅里叶变换
+        signal_fft_abs = np.abs(signal_fft)     # 取模
+
+        # 换算成实际的模值
+        signal_fft_abs = signal_fft_abs / (self.N/2)
+        # signal_fft_abs[0] = signal_fft_abs[0] / 2
 
         plt.subplot(self.x, self.y, self.count)
         self.count = self.count + 1
 
-        plt.plot(self.t, np.abs(signal_fft)), plt.title(title)
+        plt.plot(self.t_s, signal_fft_abs), plt.title(title)
 
     
