d4e06d2e3f719694e89394b9c474874bbd3ba8b0
yinda <da_yin@139.com>
2020-09-29 22:53:50 +0800

    调制函数

--- modulation.py
+++ modulation.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from signal_display import display_signal
+from scipy import fftpack
 
 class modulation:
     def  __init__(self, fs, N):
@@ -15,6 +16,8 @@ class modulation:
         self.t_s = np.linspace(-N//2, N//2, N) * fs/N   # 频域坐标
         self.s_m = self.t
         self.s_c = self.t
+        self.fm = 0
+        self.fc = 0
     
     def modula_signal(self, fm, Am):
         '''
@@ -23,6 +26,7 @@ class modulation:
         @param Am: 调制信号幅度
         @return: 基带调制信号
         '''
+        self.fm = fm
         self.s_m = Am * np.cos(2 * np.pi * fm * self.t)
         return self.s_m
 
@@ -33,6 +37,7 @@ class modulation:
         @param Ac: 载波幅度
         @return: 载波信号
         '''
+        self.fc = fc
         self.s_c = Ac * np.cos(2 * np.pi * fc * self.t)
         return self.s_c
     
@@ -42,8 +47,13 @@ class modulation:
         @param A0: 直流偏置
         @return: 调幅信号
         '''
-        s_Am = (A0 + self.s_m) * self.s_c
-        return s_Am
+        s_AM = (A0 + self.s_m) * self.s_c
+
+        # 显示信号
+        AM_show = display_signal(self.fs, self.N, 2, 1)
+        AM_show.display_time(s_AM, 'AM signal')
+        AM_show.display_sprcturm(s_AM, 'spectrum of AM signal')
+        return s_AM
 
     def DSB_Modulation(self):
         """
@@ -51,13 +61,20 @@ class modulation:
         @return: 双边带信号
         """
         s_DSB = self.s_m * self.s_c
+
+        # 显示信号
+        DSB_show = display_signal(self.fs, self.N, 2, 1)
+        DSB_show.display_time(s_DSB, 'DSB signal')
+        DSB_show.display_sprcturm(s_DSB, 'spectrum of DSB signal')
         return s_DSB
 
-    def SSB_Modulation_filter(self, fc):
+    def SSB_Modulation_filter(self):
         '''
         单边带调制——滤波法
         @param fc: 载波频率
         '''
+        fc = self.fc
+
         s_DSB = self.DSB_Modulation()        # 产生一个双边带信号
         s_fft = np.fft.fftshift(np.fft.fft(s_DSB))  # 傅里叶变换
 
@@ -69,13 +86,36 @@ class modulation:
                 H[i] = 0
 
         SSB_fft = s_fft * H
+        s_SSB = np.fft.ifft(SSB_fft)
 
-        plt.figure()
-        plt.subplot(311), plt.plot(t_s, np.abs(s_fft))
-        plt.subplot(312), plt.plot(t_s, H)
-        plt.subplot(313), plt.plot(t_s, np.abs(SSB_fft))
-        return SSB_fft
+        # 显示信号
+        SSB_show = display_signal(self.fs, self.N, 2, 2, title='filter method of SSB')
+        SSB_show.specturm_display(s_fft, 'spectrum of SSB signal')
+        SSB_show.specturm_display(H, 'Filter')
+        SSB_show.specturm_display(SSB_fft, 'filted specturm of signal')
+        SSB_show.display_time(s_SSB, 'SSB signal')
+        return s_SSB
 
+    def SSB_Modulation_shift(self):
+        '''
+        单边带调制相移法
+        '''
+        s_m = self.s_m
+        fc = self.fc
+        t = self.t
+
+        # 对调制信号进行希尔伯特变换
+        h_s = fftpack.hilbert(s_m)  # 希尔伯特变换后的时域信号
+        
+        # 构建SSB信号
+        s_SSB = 0.5*s_m*np.cos(2*np.pi*fc*t) + 0.5*h_s*np.sin(2*np.pi*fc*t)
+
+        # 显示信号
+        SSB_show = display_signal(self.fs, self.N, 2, 1)
+        SSB_show.display_time(s_SSB, 'SSB signal')
+        SSB_show.display_sprcturm(s_SSB, 'specturm of SSB signal')
+        return s_SSB
+        
 
 
 if __name__ == "__main__":
@@ -90,16 +130,10 @@ if __name__ == "__main__":
     fc = 64     # 载波频率
     Ac = 2      # 载波信号幅度
     signal_carried = modula.carried_signal(fc, Ac)
-    
-    A0 = 2     # 直流偏量，若直流偏量小于调制信号幅度，则会出现“过调幅”现象，无法包络检波
-    modula_AM = modula.Amplitude_Modulation(A0)     # 常规双边带调制，AM
-
-    modula_DSB = modula.DSB_Modulation()   # 抑制载波双边带信号，DSB，全部功率用于信息传输
-
-    modula.SSB_Modulation_filter(fc)
 
     t_t = np.linspace(0, N/fs, N)       # 时域坐标
     t_s = np.linspace(0, N, N) * fs/N   # 频域坐标
+
     # 显示调制信号和载波
     signal_show = display_signal(fs, N, 2, 2, title='Raw Signal')
     signal_show.display_time(signal_modula, title='modulated signal')
@@ -107,18 +141,18 @@ if __name__ == "__main__":
     signal_show.display_time(signal_carried, title='carried signal')
     signal_show.display_sprcturm(signal_carried, title='specturm of carrried signal')
     
-    # 显示AM信号
-    AM_show = display_signal(fs, N, 2, 1)
-    AM_show.display_time(modula_AM, 'AM signal')
-    AM_show.display_sprcturm(modula_AM, 'spectrum of AM signal')
+    # 创建AM信号
+    A0 = 2     # 直流偏量，若直流偏量小于调制信号幅度，则会出现“过调幅”现象，无法包络检波
+    modula_AM = modula.Amplitude_Modulation(A0)     # 常规双边带调制，AM
 
-    # 显示DSB信号
-    DSB_show = display_signal(fs, N, 2, 1)
-    DSB_show.display_time(modula_DSB, 'DSB signal')
-    DSB_show.display_sprcturm(modula_DSB, 'spectrum of DSB signal')
+    # 创建DSB信号
+    modula_DSB = modula.DSB_Modulation()   # 抑制载波双边带信号，DSB，全部功率用于信息传输
 
-    # 显示SSB信号
-    # SSB_show = display_signal(fs, N, 2, 1)
-    # DSB_show.display_time(modula_SSB, 'DSB signal')
-    # DSB_show.display_sprcturm(modula_SSB, 'spectrum of DSB signal')
+    # 创建SSB信号
+    # 1、滤波法
+    modula_SSB_filter = modula.SSB_Modulation_filter()
+    # 2、相移法
+    modula_SSB_shift = modula.SSB_Modulation_shift()
+
     plt.show()
+
--- signal_display.py
+++ signal_display.py
@@ -23,6 +23,10 @@ class display_signal:
     
     def display_sprcturm(self, signal, title=''):
         signal_fft = np.fft.fftshift(np.fft.fft(signal))    # 傅里叶变换
+        self.specturm_display(signal_fft, title=title)
+
+
+    def specturm_display(self, signal_fft, title=''):
         signal_fft_abs = np.abs(signal_fft)     # 取模
 
         # 换算成实际的模值
